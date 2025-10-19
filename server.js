13898c652f37974b82979ec33c8d66fe40f0f4fa
niklasseifert4523-lgtm <niklasseifert4523@gmail.com>
2025-10-19 10:34:33 +0200

    Update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server.listen(PORT, () => console.log(`Backend läuft auf Port ${PORT}`));
 
 
 app.use(express.json());
-app.use(cors({ origin: 'https://chase-the-chaser.netlify.app', credentials: true }));
+app.use(cors({ origin: 'https://chase-the-chaser1.netlify.app', credentials: true }));
 
 
 mongoose.connect('mongodb+srv://CTC:1234@ctc.6sdysob.mongodb.net/?retryWrites=true&w=majority&appName=CTC');
